f854166ec232ef5724f950ef2a1ccf7f279f9bf2
lanceparlier <lance.parlier@gmail.com>
2024-03-19 14:35:58 -0400

    Add code coverage to backend tests

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -5,7 +5,8 @@
 ##
 
 docker-compose --env-file releasecab_api/environments/env.dev -f docker-compose-arm.dev.yml up -d 
-docker-compose --env-file releasecab_api/environments/env.dev -f docker-compose-arm.dev.yml exec web sh -c "cd releasecab_api && python manage.py test"
+docker-compose --env-file releasecab_api/environments/env.dev -f docker-compose-arm.dev.yml exec web sh -c \
+  "cd releasecab_api && coverage run --omit=*/__init__.py,manage.py,*/api_root.py,*/migrations/*,*/tests/*,*/settings.py,*/urls.py manage.py test && coverage report"
 sleep 2
 docker-compose --env-file releasecab_api/environments/env.dev -f docker-compose-arm.dev.yml stop
 docker-compose --env-file releasecab_api/environments/env.dev -f docker-compose-arm.dev.yml down -v
